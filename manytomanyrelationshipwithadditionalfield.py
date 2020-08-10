70168a9929c544f86bbba6aeb189fffd9d54f963
Manoj Gautam <nix1947@users.noreply.github.com>
2020-08-10 08:06:47 +0545

    Create manytomanyrelationshipwithadditionalfield.py

---
+++ manytomanyrelationshipwithadditionalfield.py
@@ -0,0 +1,22 @@
+# Stackoverflo: https://stackoverflow.com/questions/4443190/djangos-manytomany-relationship-with-additional-fields
+
+from django.db import models
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person)
+    group = models.ForeignKey(Group)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
